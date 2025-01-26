a45239ab7daa248310d9ba70416428419fc19f53
MutableTuple <mutabletuple39@gmail.com>
2025-01-26 14:25:23 +0530

    added gtag

--- app/page.js
+++ app/page.js
@@ -7,14 +7,3 @@ export default function Home() {
     </>
   );
 }
-/*
-
- <meta
-          name="description"
-          content="Get the latest Bitcoin price, news, charts, and more. Stay updated with Bitcoin-related information including its value, prediction, and mining."
-        />
-        <meta
-          name="keywords"
-          content="bitcoin price, bitcoin price usd, bitcoin price today, bitcoin news, price of bitcoin, bitcoin etf, bitcoin halving, bitcoin stock, bitcoin to usd, bitcoin usd, bitcoin value, bitcoin price prediction, bitcoin cash, bitcoin mining, bitcoin price live, bitcoin price chart, bitcoin chart, bitcoin halving countdown, what is bitcoin, probo, probo app, probo app download, probo apk, probo apk download, probo referral code, probo mod apk download, probo app download play store, probo download, probo mod apk, probo app download new version, www probo in, probo app download apk, probo apk download new version, probo app download android, probo referral code 2024, probo app download free, probo ऐप डाउनलोड, probo login, probo app real or fake"
-        />
-*/
